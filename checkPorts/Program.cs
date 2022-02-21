22785ab767a46c0a04381988d3f9f43f935f0bc5
isaac <gusilight80@gmail.com>
2022-02-21 09:18:57 +0100

    Add project files.

---
+++ checkPorts/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace checkPorts
+{
+    using System;
+    using System.Net.Sockets;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    // client = Client(('108.128.24.171', 11211))
+                    tcpClient.Connect("108.128.24.171", 11211);
+                    Console.WriteLine("Port open");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Port closed");
+                }
+            }
+        }
+    }
+}
